8317d3692d48033b0229d297246f4adf7eb4be26
kennethowusu <kenneth@cyboticx.com>
2024-09-29 17:58:30 +0000

    fix: Allowed host

--- acci/settings/staging.py
+++ acci/settings/staging.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from acci.settings.base import *
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22"]
 CORS_ALLOWED_ORIGINS = [
     'https://local.scanport.com',
     'http://stage.accihq.org',
