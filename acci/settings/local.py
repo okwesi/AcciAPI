07c3458e08dbcf6d2100accf77cd945c8bb96964
kennethowusu <kenneth@cyboticx.com>
2024-11-22 23:48:53 +0000

    Add allowed host

--- acci/settings/local.py
+++ acci/settings/local.py
@@ -4,7 +4,7 @@ from acci.settings.base import *
 
 DEBUG = env('DEBUG', cast=bool)
 # ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*', cast=Csv())
-ALLOWED_HOSTS = ('localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000")
+ALLOWED_HOSTS = ('localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000", "backend.accihq.org")
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://0.0.0.0:8000',
@@ -20,7 +20,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=120),
 }
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", 'https://backend.accihq.org']
 STATIC_URL = f'/static/'
 # Media files
 MEDIA_URL = f'/media/'
