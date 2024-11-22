672ff8230bb64dcb8d19775f14f947ab550b37b6
kennethowusu <kenneth@cyboticx.com>
2024-11-22 23:57:49 +0000

    Add allowed host 3

--- acci/settings/base.py
+++ acci/settings/base.py
@@ -173,6 +173,13 @@ INTERNAL_HOST = env('INTERNAL_HOST')
 ASGI_APPLICATION = 'acci.asgi.application'
 
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000',
+    'http://localhost:3000',
+    'https://backend.accihq.org',  # Add this if missing
+]
 
 
 
--- acci/settings/local.py
+++ acci/settings/local.py
@@ -4,7 +4,7 @@ from acci.settings.base import *
 
 DEBUG = env('DEBUG', cast=bool)
 # ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*', cast=Csv())
-ALLOWED_HOSTS = ('localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000", "backend.accihq.org")
+ALLOWED_HOSTS = ("*")
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://0.0.0.0:8000',
