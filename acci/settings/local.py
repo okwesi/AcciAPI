f5d52d07dca00f5f44f27cb580c8f7e33990672a
kennethowusu <kenneth@cyboticx.com>
2024-11-23 00:02:22 +0000

    Add allowed host 5

--- acci/settings/local.py
+++ acci/settings/local.py
@@ -4,17 +4,18 @@ from acci.settings.base import *
 
 DEBUG = env('DEBUG', cast=bool)
 # ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*', cast=Csv())
-ALLOWED_HOSTS = ('localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000", "backend.accihq.org")
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://0.0.0.0:8000',
-    'http://127.0.0.1:0000',
-    'http://127.0.0.1:3000',
-    'http://0.0.0.0:3000',
-    'http://localhost:3000',
-    "https://backend.accihq.org",
-
-]
+ALLOWED_HOSTS = ("*")
+# ALLOWED_HOSTS = ('localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000", "backend.accihq.org")
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000',
+#     'http://0.0.0.0:8000',
+#     'http://127.0.0.1:0000',
+#     'http://127.0.0.1:3000',
+#     'http://0.0.0.0:3000',
+#     'http://localhost:3000',
+#     "https://backend.accihq.org",
+#
+# ]
 DEBUG = True
 ENVIRONMENT = env('ENVIRONMENT')
 SIMPLE_JWT = {
@@ -22,11 +23,11 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=120),
 }
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "https://backend.accihq.org",
-    "http://localhost:8000",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://backend.accihq.org",
+#     "http://localhost:8000",
+# ]
 CSRF_COOKIE_SECURE = True
 STATIC_URL = f'/static/'
 # Media files
