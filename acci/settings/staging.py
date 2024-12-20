958ad943ca2017ece51b34d1668d86b1529a043c
kennethowusu <kenneth@cyboticx.com>
2024-12-20 23:10:49 +0000

    stage admin fix

--- acci/settings/staging.py
+++ acci/settings/staging.py
@@ -2,12 +2,13 @@ from datetime import timedelta
 
 from acci.settings.base import *
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22", "backend.accihq.org"]
+ALLOWED_HOSTS = ("*")
 CORS_ALLOWED_ORIGINS = [
     'https://local.scanport.com',
     'https://stage.accihq.org',
     'http://158.178.200.22:8000',
     'http://localhost:3000',
+    "https://backend.accihq.org",
 ]
 DEBUG = True
 ENVIRONMENT = env('ENVIRONMENT')
@@ -16,7 +17,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=120),
 }
-CSRF_TRUSTED_ORIGINS = ['https://stage.accihq.org', 'http://158.178.200.22:3000',]
+CSRF_TRUSTED_ORIGINS = ['https://stage.accihq.org', 'http://158.178.200.22:3000', "https://backend.accihq.org", ]
 STATIC_URL = f'/static/'
 # Media files
 MEDIA_URL = f'/media/'
