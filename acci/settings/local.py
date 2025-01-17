edb084196d699ae169d3f60e77165a695f59bc0a
kennethowusu <kenneth@cyboticx.com>
2025-01-17 23:42:26 +0000

    Add s3 bucket to the staging for testing

--- acci/settings/local.py
+++ acci/settings/local.py
@@ -1,3 +1,6 @@
+from acci.settings.base import *
+
+from datetime import timedelta
 from datetime import timedelta
 
 from acci.settings.base import *
@@ -5,30 +8,34 @@ from acci.settings.base import *
 DEBUG = env('DEBUG', cast=bool)
 # ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*', cast=Csv())
 ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = ('localhost', '127.0.0.1', '0.0.0.0', "158.178.200.22:8000", "backend.accihq.org")
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://0.0.0.0:8000',
-    'http://127.0.0.1:0000',
-    'http://127.0.0.1:3000',
-    'http://0.0.0.0:3000',
-    'http://localhost:3000',
-    "https://backend.accihq.org",
-
-]
-DEBUG = True
 ENVIRONMENT = env('ENVIRONMENT')
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=120),
 }
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "https://backend.accihq.org",
-    "http://localhost:8000",
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
-CSRF_COOKIE_SECURE = True
-STATIC_URL = f'/static/'
-# Media files
-MEDIA_URL = f'/media/'
+# AWS S3 configuration
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', None)
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', None)
+CSRF_TRUSTED_ORIGINS = [
+    "http://*",
+    "https://*"
+]
+# extra static and media file settings.
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+
+
+
+# STATIC_URL = f'/static/'
+# # Media files
+# MEDIA_URL = f'/media/'
+
+
--- acci/settings/staging.py
+++ acci/settings/staging.py
@@ -19,6 +19,17 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(days=120),
 }
 CSRF_TRUSTED_ORIGINS = ['https://stage.accihq.org', 'http://158.178.200.22:3000', "https://backend.accihq.org", ]
-STATIC_URL = f'/static/'
-# Media files
-MEDIA_URL = f'/media/'
+# STATIC_URL = f'/static/'
+# # Media files
+# MEDIA_URL = f'/media/'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', None)
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', None)
+
+# extra static and media file settings.
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
